a35dd0d11735d6d2cfe2a265680f48d17369a7c7
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-07-20 15:18:33 -0700

    got the file header to read to the console.

--- bitmap-autobots/lib/bitmap.js
+++ bitmap-autobots/lib/bitmap.js
@@ -1,24 +1,21 @@
 'use strict';
 
 const fs = require('fs');
+module.exports = Bitmap;
 
 function Bitmap(filepath) {
-  fs.readFile(`${__dirname}/..assets/palette-bitmap.bmp`, function(err, buffer) {
+  fs.readFile(filepath, (err, buffer) => {
     if (err) throw err;
-    console.log(buffer);
     this.buffer = buffer;
-    readHeader();
+    readHeader.call(this);
+
   });
 
   function readHeader() {
-    this.type = buffer.toString('utf-8', 0, 2);
-    this.size = buffer.readInt32LE(2);
-    this.reserved1 = buffer.readInt32LE(6);
-    this.reserved2 = buffer.readInt32LE(8);
-    this.fileOffset = buffer.readInt32LE(10);
-  };
-
-  function readDibHeader() {
-
-  };
-};
+    this.type = this.buffer.toString('utf-8', 0, 2);
+    this.size = this.buffer.readInt32LE(2);
+    this.reserved1 = this.buffer.readInt32LE(6);
+    this.reserved2 = this.buffer.readInt32LE(8);
+    this.pixelArrayOffset = this.buffer.readInt32LE(10);
+  }
+}
