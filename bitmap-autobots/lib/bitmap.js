a6d2e201b67c6f5270cffab0250fe8dcc982d723
Bret Ldenburg <bret.ladenburg@gmail.com>
2017-07-20 16:20:15 -0700

    wrote some logic to read the hex numbers of the color table

--- bitmap-autobots/lib/bitmap.js
+++ bitmap-autobots/lib/bitmap.js
@@ -7,15 +7,26 @@ function Bitmap(filepath) {
   fs.readFile(filepath, (err, buffer) => {
     if (err) throw err;
     this.buffer = buffer;
-    readHeader.call(this);
+    readHeader.call(this, buffer);
+    readColorTable.call(this);
 
   });
 
-  function readHeader() {
-    this.type = this.buffer.toString('utf-8', 0, 2);
-    this.size = this.buffer.readInt32LE(2);
-    this.reserved1 = this.buffer.readInt32LE(6);
-    this.reserved2 = this.buffer.readInt32LE(8);
-    this.pixelArrayOffset = this.buffer.readInt32LE(10);
+  function readHeader(buffer) {
+    this.type = buffer.toString('utf-8', 0, 2);
+    this.size = buffer.readInt32LE(2);
+    this.reserved1 = buffer.readInt32LE(6);
+    this.reserved2 = buffer.readInt32LE(8);
+    this.pixelArrayOffset = buffer.readInt32LE(10);
+  }
+
+  function readColorTable() {
+    this.colors = [];
+    var colorTableOffset = 14 + this.dibHeaderSize;
+    var colorTableSize = this.colors * 4;
+    for(var i = colorTableOffset; i <= colorTableOffset + colorTableSize; i+=4) {
+      var colorHexString = this.buffer.toString('hex', i, i + 4);
+      this.colors.push(colorHexString);
+    }
   }
 }
