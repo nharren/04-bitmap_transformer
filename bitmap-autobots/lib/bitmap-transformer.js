90a734c4471ff5b1e2e0f519e2b6a88404607ea7
sharmarke <smarky292@gmail.com>
2017-07-21 15:19:01 -0700

    added invert transform

---
+++ bitmap-autobots/lib/bitmap-transformer.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const bitmap = require('./lib/bitmap.js');
+
+createBitmap(`${__dirname}/assets/palette-bitmap.bmp`, function(err, bitmap) {
+  console.log(bitmap);
+});
+
+function invert(colors) {
+  for (var i = 0; i < colors.length; i++) {
+    var color = colors[i];
+    var r = 255-parseInt(color.slice(0, 2), 16);
+    var g = 255-parseInt(color.slice(2, 4), 16);
+    var b = 255-parseInt(color.slice(4, 6), 16);
+    var a = 255-parseInt(color.slice(6, 8), 16);
+
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    a = a.toString(16);
+
+    colors[i] = (r+g+b+a);
+
+  }
+  return colors;
+}
