940c654d3b2bb6d70c00b34dec210c7ca1b42b40
sharmarke <smarky292@gmail.com>
2017-07-21 15:47:02 -0700

    adds grayscale transformation

--- bitmap-autobots/lib/bitmap-transformer.js
+++ bitmap-autobots/lib/bitmap-transformer.js
@@ -24,3 +24,26 @@ function invert(colors) {
   }
   return colors;
 }
+
+function grayscale(colors) {
+  for (var i = 0; i < colors.length; i++) {
+    var color = colors[i];
+    var r = parseInt(color.slice(0, 2), 16);
+    var g = parseInt(color.slice(2, 4), 16);
+    var b = parseInt(color.slice(4, 6), 16);
+    var a = parseInt(color.slice(6, 8), 16);
+
+    r,g,b = (r+g+b)/3;
+
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    a = a.toString(16);
+
+    colors[i] =(r+g+b+a);
+
+  }
+  return colors;
+}
+
+}
